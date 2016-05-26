899cdab5df83faba9802ffed4201bb1e589b7b79
Raphael Parree <rparree@edc4it.com>
2016-05-26 14:15:54 +0200

    Added the HtmlWebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: "./app.js",
     output: {
         path: "./dist",
-        filename: "[name].js" 
-    }
+        filename: "[name].js"
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            inject : "body"
+        })
+    ]
 };
