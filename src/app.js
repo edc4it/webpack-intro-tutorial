ebedcb5165975d194660ccea261e4bbb7d1be5a6
Raphael Parree <rparree@edc4it.com>
2016-05-26 15:43:39 +0200

    Added simple reference to jquery

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 var msg = require("./contents.js");
 require("./message.css");
-var div = document.createElement("div");
-div.id="message";
-var txtNode = document.createTextNode(msg);
-div.appendChild(txtNode);
-document.body.appendChild(div);
+var $ = require("jquery");
+$(function(){
+    $("<div id='message'>")
+        .text(msg+"!")
+        .appendTo("body");
+});
 
