4f50344e5297a661f6bbf2b7b7b29ec3509cdb63
Raphael Parree <rparree@edc4it.com>
2018-10-12 12:36:29 +0200

    adjusted for webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.ts']
-    },
-    entry: {
-        main: "./src/app.ts",
-        vendor: "./src/vendor.js"
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'), // output directory
-        filename: "[name].js" // name of the generated bundle
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.ts$/,
-                loader: "awesome-typescript-loader"
-            },
-            {
-                test: /\.ts$/,
-                enforce: "pre",
-                loader: 'tslint-loader'
-            },
-            {
-                test: /\.scss$/,
-                loader: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "src/index.html",
-            inject: "body"
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        })
+  mode: 'development',	
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  entry: {
+    main: "./src/index.ts",
+    vendor: "./src/vendor.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), // output directory
+    filename: "[name].js" // name of the generated bundle
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.ts$/,
+        enforce: "pre",
+        loader: 'tslint-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"]
+      }
     ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: 'vendor',
+        },
+      },
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: "body"
+    })
+  ]
 };
