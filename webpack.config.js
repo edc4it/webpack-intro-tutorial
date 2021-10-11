94ece973434afb6e1e1ed5457ac8aebc2ae85a49
Raphael Parree <rparree@edc4it.com>
2021-10-11 13:45:24 +0200

    feat: updated to webpack 5.58

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts', // bundle's entry point
-  output: {
-    path: path.resolve(__dirname, 'dist'), // output directory
-    filename: '[name].js', // name of the generated bundle
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-    }),
-    new ESLintPlugin({
-      extensions: ['ts', 'tsx'],
-    }),
-  ],
+    entry: "./src/index.ts", // bundle's entry point
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'), // output directory
+        filename: "[name].js" // name of the generated bundle
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject : "body",
+            scriptLoading: "blocking"
+        })
+    ]
 };
